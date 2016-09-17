3c94f0a31b4c7564b8c5ac3644cfe55123ce23c0
Jógvan Olsen <jo@jebster.net>
2016-09-17 14:31:08 +0100

    Small fixes

--- app/src/test/java/net/jebster/TornNotifications/TornNotificationManagerTest.java
+++ app/src/test/java/net/jebster/TornNotifications/TornNotificationManagerTest.java
@@ -34,8 +34,7 @@ public class TornNotificationManagerTest {
     }
 
     @Test
-    public void notificationsCheck_TravelNotificationCalledTest(){
-        when(sampleSaveData.getSound()).thenReturn(null);
+    public void notificationsCheck_EnergyNotificationCalledTest(){
         when(sampleSaveData.EnergyNotification()).thenReturn(true);
         when(sampleTornUser.getEnergy()).thenReturn(145);
         when(sampleTornUserFull.getEnergy()).thenReturn(150);
@@ -49,7 +48,24 @@ public class TornNotificationManagerTest {
 
         tnm.notificationsCheck(sampleSaveData, sampleTornUserFull, sampleTornUser);
 
-
         verify(tnm).EnergyNotification();
+    }
+
+    @Test
+    public void notificationsCheck_EnergyNotificationNotCalledTest(){
+        when(sampleSaveData.EnergyNotification()).thenReturn(true);
+        when(sampleTornUser.getEnergy()).thenReturn(150);
+        when(sampleTornUserFull.getEnergy()).thenReturn(150);
+        when(sampleTornUserFull.getMaximumEnergy()).thenReturn(150);
+
+        doNothing().when(tnm).EnergyNotification();
+        doNothing().when(tnm).HappyNotification();
+        doNothing().when(tnm).NerveNotification();
+        doNothing().when(tnm).TravelNotification();
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        tnm.notificationsCheck(sampleSaveData, sampleTornUserFull, sampleTornUser);
+
+        verify(tnm, never()).EnergyNotification();
     }
 }
