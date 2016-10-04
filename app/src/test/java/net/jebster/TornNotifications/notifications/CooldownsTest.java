9eab29b18ef9fa298fb010ed702ed79803ee3794
Jógvan Olsen <jo@jebster.net>
2016-10-04 21:52:01 +0100

    more cooldowns tests

--- app/src/test/java/net/jebster/TornNotifications/notifications/CooldownsTest.java
+++ app/src/test/java/net/jebster/TornNotifications/notifications/CooldownsTest.java
@@ -58,4 +58,209 @@ public class CooldownsTest {
         verify(tnm, times(1)).DrugNotification();
     }
 
+    @Test
+    public void notificationCheck_CooldownDrugFinishedSettingDisabled_CooldownsNotificationNotCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(false);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(11,0,0));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(0)).DrugNotification();
+    }
+
+    @Test
+    public void notificationCheck_CooldownDrugAlreadyFinished_CooldownsNotificationNotCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(true);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(0)).DrugNotification();
+    }
+
+    @Test
+    public void notificationCheck_CooldownDrugStillOn_CooldownsNotificationNotCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(true);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(125,0,0));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(105,0,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(0)).DrugNotification();
+    }
+
+
+
+
+    /****************************************/
+    /*                                      */
+    /*            Medical Tests             */
+    /*                                      */
+    /****************************************/
+
+    @Test
+    public void notificationCheck_CooldownMedicalFinished_CooldownsNotificationCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(true);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,11,0));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(1)).MedicalNotification();
+    }
+
+    @Test
+    public void notificationCheck_CooldownMedicalFinishedSettingDisabled_CooldownsNotificationNotCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(false);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,11,0));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(0)).MedicalNotification();
+    }
+
+    @Test
+    public void notificationCheck_CooldownMedicalAlreadyFinished_CooldownsNotificationNotCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(true);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(0)).MedicalNotification();
+    }
+
+    @Test
+    public void notificationCheck_CooldownMedicalStillOn_CooldownsNotificationNotCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(true);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,125,0));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,105,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(0)).MedicalNotification();
+    }
+
+
+
+
+    /****************************************/
+    /*                                      */
+    /*           Boosters Tests             */
+    /*                                      */
+    /****************************************/
+
+    @Test
+    public void notificationCheck_CooldownBoostersFinished_CooldownsNotificationCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(true);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,11));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(1)).BoosterNotification();
+    }
+
+    @Test
+    public void notificationCheck_CooldownBoostersFinishedSettingDisabled_CooldownsNotificationNotCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(false);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,11));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(0)).BoosterNotification();
+    }
+
+    @Test
+    public void notificationCheck_CooldownBoostersAlreadyFinished_CooldownsNotificationNotCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(true);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,0));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(0)).BoosterNotification();
+    }
+
+    @Test
+    public void notificationCheck_CooldownBoostersStillOn_CooldownsNotificationNotCalled(){
+        // Arrange
+        when(sampleSaveData.CooldownNotification()).thenReturn(true);
+        when(sampleLastUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,125));
+        when(sampleNowUser.getCooldowns()).thenReturn(new TornUser.Cooldown(0,0,105));
+
+        nothing(tnm);
+        doCallRealMethod().when(tnm).notificationsCheck(any(SaveData.class), any(TornUser.class), any(TornUser.class));
+
+        // Act
+        tnm.notificationsCheck(sampleSaveData, sampleNowUser, sampleLastUser);
+
+        // Assert
+        verify(tnm, times(0)).BoosterNotification();
+    }
+
 }
